115d830cc7bec4067d1c0dab60621d6edf467941
MeiJun63 <115607488+MeiJun63@users.noreply.github.com>
2022-11-16 23:21:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ selectMenu=document.querySelectorAll("select"),
 setAlarmBtn=document.querySelector("button");
 
 let alarmTime, isAlarmSet=false,
-ringtone= new Audio("./files/ringtone.mp3");
+ringtone= new Audio("ringtone.mp3");
 
 for (let i=12;i>0;i--)
 {
-    i=i<10?"0"+1:i;
-    let option= '<option value="${i}">${i}</option>  ';
+    i=i<10?"0"+i:i;
+    let option= `<option value="${i}">${i}</option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
 }
 for (let i=59;i>0;i--)
 {
-    i=i<10?"0"+1:i;
-    let option= '<option value="${i}">${i}</option>  ';
+    i=i<10?"0"+i:i;
+    let option= `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML("afterend",option);
 }
 for (let i=2;i>0;i--)
 {
     let ampm= i==1?"AM":"PM";
-    let option= '<option value="${ampm}">${ampm}</option>';
+    let option= `<option value="${ampm}">${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend",option);
 }
 
@@ -44,15 +44,15 @@ setInterval(()=>{
     m=m<10?"0"+m:m;
     s=s<10?"0"+s:s;
 
-    currentTime.innerText('${h}:${m}:${s} ${ampm}')
+    currentTime.innerText=`${h}:${m}:${s} ${ampm}`;
 
-    if(alarmTime=="${h}:${m}:${s} ${ampm}")
+    if(alarmTime==`${h}:${m}:${s} ${ampm}`)
     {
         ringtone.play();
         ringtone.loop=true;
     }
 
-},1000)
+},1000);
 
 function setAlarm(){
     if(isAlarmSet)
@@ -65,7 +65,7 @@ function setAlarm(){
     }
 
     //getting hour, minute,ampm select tag value
-    let time="${selectMenu[0].value}:${selectMenu[1].value}:${selectMenu[2].value}";
+    let time=`${selectMenu[0].value}:${selectMenu[1].value}:${selectMenu[2].value}`;
     if(time.includes("Hour")||time.includes("Minute")||time.includes("AM/PM")){
         return alert("Please, select a valid time to set Alarm!");
     }
